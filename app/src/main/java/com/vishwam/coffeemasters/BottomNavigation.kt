7bbac0847625d5266244a044e62a54e6e7f1b154
Vishwam1158 <vishwam1158@gmail.com>
2024-03-06 12:18:44 +0530

    adding Bottom bar with Icons

--- app/src/main/java/com/vishwam/coffeemasters/BottomNavigation.kt
+++ app/src/main/java/com/vishwam/coffeemasters/BottomNavigation.kt
@@ -1,7 +1,12 @@
 package com.vishwam.coffeemasters
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -13,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,13 +38,37 @@ object Routes {
     val pages = listOf(MenuPage, OffersPage, OrderPage, InfoPage)
 }
 
-// for preview any one page 
+// for just preview any one page it will not affect in the app 
 @Preview
 @Composable
 fun NavBarItem_Preview() {
-    NavBarItem(page = Routes.MenuPage)
+    NavBarItem(page = Routes.MenuPage, modifier = Modifier.padding(8.dp))
 }
 
+
+@Composable
+fun NavBar(selectedRoute: String = Routes.MenuPage.route,
+           onChange: (String)->Unit) {
+    Row(
+        //apply style for hiding the overlaping of navbar with content of app and fill icons in the whole width
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF43281C))  // set primary here instead of code
+            .padding(16.dp) // here we give padding for increase the size of bottom nav bar and give padding after the background
+    ) {
+        for (page in Routes.pages) {
+            NavBarItem(
+                page = page,
+                selected = selectedRoute == page.route,
+                modifier = Modifier.clickable {
+                    onChange(page.route)
+                }
+            )
+        }
+    }
+    
+}
 
 @Composable
 fun NavBarItem(page: NavPage, selected: Boolean = false, modifier: Modifier = Modifier) {
@@ -48,7 +78,7 @@ fun NavBarItem(page: NavPage, selected: Boolean = false, modifier: Modifier = Mo
             imageVector = page.icon,
             contentDescription = page.name,
             colorFilter = ColorFilter.tint(
-                if (selected) Alternative1 else OnPrimary
+                if (selected) OnPrimary else Alternative1
             ),
             modifier = Modifier
                 .padding(bottom = 8.dp)
@@ -56,7 +86,7 @@ fun NavBarItem(page: NavPage, selected: Boolean = false, modifier: Modifier = Mo
         )
         Text(page.name,
             fontSize = 12.sp,
-            color = if (selected) Alternative1 else OnPrimary
+            color = if (selected) OnPrimary else Alternative1
         )
     }
 }
