b5accc78ff53d0c9ae2794b4af091509b52c25fc
Vishwam1158 <vishwam1158@gmail.com>
2024-03-08 12:30:35 +0530

    Add classes

---
+++ app/src/main/java/com/vishwam/coffeemasters/DataModel.kt
@@ -0,0 +1,10 @@
+package com.vishwam.coffeemasters
+
+class Product(var id: Int, var name: String, var price: Double, var image: String ){
+
+    val imageUrl get() = "https://firtman.github.io/coffeemasters/api/images/${this.image}"
+}
+
+class Category(var name: String, var products: MutableList<Product>)
+
+class ItemInCart(var product: Product, var quantity: Int)
