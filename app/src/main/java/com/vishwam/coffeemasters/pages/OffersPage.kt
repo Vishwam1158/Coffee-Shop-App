1bdceac0d2d83aa47d873054d0a939b68042012d
Vishwam1158 <vishwam1158@gmail.com>
2024-03-08 12:36:14 +0530

    Remove unnecessary code

--- app/src/main/java/com/vishwam/coffeemasters/pages/OffersPage.kt
+++ app/src/main/java/com/vishwam/coffeemasters/pages/OffersPage.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
