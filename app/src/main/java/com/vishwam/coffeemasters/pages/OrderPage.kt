5ca9ebea9d385bf51247fdbcda7e65e9852dd33a
Vishwam1158 <vishwam1158@gmail.com>
2024-03-08 12:30:56 +0530

    Create file

---
+++ app/src/main/java/com/vishwam/coffeemasters/pages/OrderPage.kt
@@ -0,0 +1,11 @@
+package com.vishwam.coffeemasters.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun OrderPage() {
+    Text(text = "order Page")
+}
