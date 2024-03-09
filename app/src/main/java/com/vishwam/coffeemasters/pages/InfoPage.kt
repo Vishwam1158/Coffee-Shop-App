73931099285b2373832c8b180be3ac3cb20eac12
Vishwam1158 <vishwam1158@gmail.com>
2024-03-09 21:05:07 +0530

    Adding WebView

--- app/src/main/java/com/vishwam/coffeemasters/pages/InfoPage.kt
+++ app/src/main/java/com/vishwam/coffeemasters/pages/InfoPage.kt
@@ -1,11 +1,34 @@
 package com.vishwam.coffeemasters.pages
 
-import androidx.compose.material3.Text
+import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 
 @Preview
 @Composable
 fun InfoPage() {
-    Text(text = "Info Page")
+    WebView()
+}
+
+@Composable
+fun WebView() {
+    // Declare a string that contains a url
+    val url = "https://firtman.github.io/coffeemasters/webapp"
+
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(factory = {
+        android.webkit.WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient() // Customize the Web View
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
 }
