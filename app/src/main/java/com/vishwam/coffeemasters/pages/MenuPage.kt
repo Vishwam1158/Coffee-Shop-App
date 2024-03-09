b6ef7861ecd1058bb881b385e4a6db4b46f19f8b
Vishwam1158 <vishwam1158@gmail.com>
2024-03-09 20:36:59 +0530

    Fetch Items through API

--- app/src/main/java/com/vishwam/coffeemasters/pages/MenuPage.kt
+++ app/src/main/java/com/vishwam/coffeemasters/pages/MenuPage.kt
@@ -1,11 +1,98 @@
 package com.vishwam.coffeemasters.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.vishwam.coffeemasters.DataManager
+import com.vishwam.coffeemasters.Product
+import com.vishwam.coffeemasters.ui.theme.Alternative1
+import com.vishwam.coffeemasters.ui.theme.CardBackground
+import com.vishwam.coffeemasters.ui.theme.Primary
 
-@Preview
 @Composable
-fun MenuPage() {
-    Text( "menu Page")
+fun MenuPage(dataManager: DataManager) {
+    LazyColumn {
+        items(dataManager.menu) {
+            Text(text = it.name,
+                color = Primary,
+                modifier = Modifier.padding(10.dp, 20.dp, 10.dp, 10.dp))
+            it.products.forEach {
+                Card(
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp), //like Z index and creating a shadow
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .background(CardBackground)
+                        .padding(12.dp)
+                ) {
+                    ProductItem(it, onAdd = {
+                        dataManager.cartAdd(it)
+                    })
+                }
+            }
+        }
+    }
+}
+
+//extending double - adding a function to every double receiving digit and converting in 2 digits.
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+
+@Composable
+fun ProductItem(product: Product, onAdd: (Product)->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = "Image for ${product.name}",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column {
+                Text(product.name, fontWeight = FontWeight.Bold)
+                Text("$${product.price.format(2)} ea")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Alternative1, // backgroundColor
+                    contentColor = Color.White
+                ),
+                onClick = {
+                    onAdd(product)
+                },
+            ) {
+                Text("Add")
+            }
+        }
+    }
 }
